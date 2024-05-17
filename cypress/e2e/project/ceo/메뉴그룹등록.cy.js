c159b3c2ed4d30c7182b228614047fe59fb15acc
gosl8908 <gosl8908@gmail.com>
2024-05-17 17:50:20 +0900

    코드 수정

--- cypress/e2e/project/ceo/메뉴그룹등록.cy.js
+++ cypress/e2e/project/ceo/메뉴그룹등록.cy.js
@@ -25,39 +25,44 @@ describe('Onprem Dashboard Test', () => {
         /* 메뉴관리 */
         cy.get(':nth-child(3) > .container-fluid > .d-flex > [href="/menu/product-div"] > .btn').click();
         cy.wait(1 * 1000);
-        cy.get('#product').click(); // 상품관리 탭
+        cy.get('[href="/menu/menu-group"] > .btn').click();
 
-        /* 분식 */
-        menuModule.menu('김밥', '2500');
-        menuModule.menu('참치김밥', '4500');
-        menuModule.menu('쫄면', '5500');
-        menuModule.menu('고기국수', '6500');
-        menuModule.menu('비빔면', '6500');
-        menuModule.menu('골뱅이무침', '9500');
-        /* 한식 */
-        menuModule.menu('비빔밥', '6500');
-        menuModule.menu('불고기', '9500');
-        /* 일식 */
-        menuModule.menu('초밥', '10500');
-        menuModule.menu('돈가스', '8000');
-        menuModule.menu('치즈돈가스', '9500');
-        /* 디저트 */
-        menuModule.menu('디저트', '3500');
-        menuModule.menu('샐러드', '8500');
-        /* 양식 */
-        menuModule.menu('피자', '19900');
-        /* 음료 */
-        menuModule.menu('코카콜라', '2500');
-        menuModule.menu('코카콜라제로', '2500');
-        menuModule.menu('펩시', '2500');
-        menuModule.menu('펩시제로', '2500');
-        menuModule.menu('스프라이트', '2500');
-        menuModule.menu('스프라이트제로', '2500');
-        /* 주류 */
-        menuModule.menu('새로', '6000');
-        menuModule.menu('진로', '6000');
-        menuModule.menu('카스', '6000');
-        menuModule.menu('테라', '6000');
+        /* 메뉴그룹 생성 */
+        cy.get('#btnAddMenuGroup').click();
+        cy.wait(1 * 1000);
+        cy.get('#category_nm').type('분식');
+        cy.get('.modal-footer > .btn-primary').click();
+        cy.get('#vueMenuGroupMain').contains('분식');
+
+        cy.get('#btnAddMenuGroup').click();
+        cy.wait(1 * 1000);
+        cy.get('#category_nm').type('일식');
+        cy.get('.modal-footer > .btn-primary').click();
+        cy.get('#vueMenuGroupMain').contains('일식');
+
+        cy.get('#btnAddMenuGroup').click();
+        cy.wait(1 * 1000);
+        cy.get('#category_nm').type('한식');
+        cy.get('.modal-footer > .btn-primary').click();
+        cy.get('#vueMenuGroupMain').contains('한식');
+
+        cy.get('#btnAddMenuGroup').click();
+        cy.wait(1 * 1000);
+        cy.get('#category_nm').type('양식');
+        cy.get('.modal-footer > .btn-primary').click();
+        cy.get('#vueMenuGroupMain').contains('양식');
+
+        cy.get('#btnAddMenuGroup').click();
+        cy.wait(1 * 1000);
+        cy.get('#category_nm').type('디저트');
+        cy.get('.modal-footer > .btn-primary').click();
+        cy.get('#vueMenuGroupMain').contains('디저트');
+
+        cy.get('#btnAddMenuGroup').click();
+        cy.wait(1 * 1000);
+        cy.get('#category_nm').type('음료');
+        cy.get('.modal-footer > .btn-primary').click();
+        cy.get('#vueMenuGroupMain').contains('음료');
     });
 
     // afterEach('Status Check', () => {
