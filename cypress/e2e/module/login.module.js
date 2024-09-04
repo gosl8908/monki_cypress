05f86ed4a8c1b890dad30f3ff70cf966ab4f4235
gosl8908 <gosl8908@gmail.com>
2024-09-04 17:49:50 +0900

    code 수정

--- cypress/e2e/module/login.module.js
+++ cypress/e2e/module/login.module.js
@@ -1,10 +1,7 @@
 function login({ Site, Type = undefined, Id, Password }) {
     cy.visit(Site, {
         timeout: 60 * 1000,
-        headers: {
-            Accept: 'application/json, text/plain, */*',
-            'User-Agent': 'axios/0.18.0',
-        },
+        passOnStatusCode: false,
     });
 
     if (Site === Cypress.env('Ceo') || Site === Cypress.env('StgCeo') || Site === Cypress.env('DevCeo')) {
