04c85f23afe8de957b7c8a089a2d1429fcbcf403
gosl8908 <gosl8908@gmail.com>
2024-09-05 12:14:28 +0900

    code 수정

--- cypress/e2e/project/test/test.cy.js
+++ cypress/e2e/project/test/test.cy.js
@@ -15,7 +15,7 @@ describe('Test', () => {
         cy.setDateToEnv();
         cy.getAll();
         loginModule.login({
-            Site: `${Cypress.env('StgCeo')}`,
+            Site: `${Cypress.env('Ceo')}`,
             Type: '단골맛집 가맹점주',
             Id: `${Cypress.env('FavTestId')[0]}`,
             Password: `${Cypress.env('TestPwd')}`,
